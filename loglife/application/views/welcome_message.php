046a8460936d6572c0c6878b2e79b72105c27792
Admin <Admin@192.168.1.2>
2013-04-14 21:44:30 +0300

    Template edit

--- loglife/application/views/welcome_message.php
+++ loglife/application/views/welcome_message.php
@@ -1,165 +1,205 @@
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Lifelog</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Lifelog">
-    <meta name="author" content="ArtyomLarichev">
+    <head>
+        <meta charset="utf-8">
+        <title>Lifelog</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Lifelog">
+        <meta name="author" content="ArtyomLarichev">
 
-    <!-- Le styles -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <style type="text/css">
-      body {
-        padding-top: 20px;
-        padding-bottom: 60px;
-      }
+        <!-- Le styles -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <style type="text/css">
+            body {
+                padding-top: 20px;
+                padding-bottom: 60px;
+            }
 
-      /* Custom container */
-      .container {
-        margin: 0 auto;
-        max-width: 1000px;
-      }
-      .container > hr {
-        margin: 60px 0;
-      }
+            .logo{
+                margin-top:-3px;
+                margin-left:25px;
+            }
 
-      /* Main marketing message and sign up button */
-      .jumbotron {
-        margin: 80px 0;
-        text-align: center;
-      }
-      .jumbotron h1 {
-        font-size: 100px;
-        line-height: 1;
-      }
-      .jumbotron .lead {
-        font-size: 24px;
-        line-height: 1.25;
-      }
-      .jumbotron .btn {
-        font-size: 21px;
-        padding: 14px 24px;
-      }
+            /* Custom container */
+            .container {
+                margin: 0 auto;
+                max-width: 1000px;
+            }
+            .container > hr {
+                margin: 60px 0;
+            }
 
-      /* Supporting marketing content */
-      .marketing {
-        margin: 60px 0;
-      }
-      .marketing p + h4 {
-        margin-top: 28px;
-      }
+            /* Main marketing message and sign up button */
+            .jumbotron {
+                margin: 80px 0;
+                text-align: center;
+            }
+            .jumbotron h1 {
+                font-size: 100px;
+                line-height: 1;
+            }
+            .jumbotron .lead {
+                font-size: 24px;
+                line-height: 1.25;
+            }
+            .jumbotron .btn {
+                font-size: 21px;
+                padding: 14px 24px;
+            }
 
-
-      /* Customize the navbar links to be fill the entire space of the .navbar */
-      .navbar .navbar-inner {
-        padding: 0;
-      }
-      .navbar .nav {
-        margin: 0;
-        display: table;
-        width: 100%;
-      }
-      .navbar .nav li {
-        display: table-cell;
-        width: 1%;
-        float: none;
-      }
-      .navbar .nav li a {
-        font-weight: bold;
-        text-align: center;
-        border-left: 1px solid rgba(255,255,255,.75);
-        border-right: 1px solid rgba(0,0,0,.1);
-      }
-      .navbar .nav li:first-child a {
-        border-left: 0;
-        border-radius: 3px 0 0 3px;
-      }
-      .navbar .nav li:last-child a {
-        border-right: 0;
-        border-radius: 0 3px 3px 0;
-      }
-    </style>
-    <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+            /* Supporting marketing content */
+            .marketing {
+                margin: 60px 0;
+            }
+            .marketing p + h4 {
+                margin-top: 28px;
+            }
 
 
-  </head>
+            /* Customize the navbar links to be fill the entire space of the .navbar */
+            .navbar .navbar-inner {
+                padding: 0;
+            }
+            .navbar .nav {
+                margin: 0;
+                display: table;
+                width: 100%;
+            }
+            .navbar .nav li {
+                display: table-cell;
+                width: 1%;
+                float: none;
+            }
+            .navbar .nav li a {
+                font-weight: bold;
+                text-align: center;
+                border-left: 1px solid rgba(255,255,255,.75);
+                border-right: 1px solid rgba(0,0,0,.1);
+            }
+            .navbar .nav li:first-child a {
+                border-left: 0;
+                border-radius: 3px 0 0 3px;
+            }
+            .navbar .nav li:last-child a {
+                border-right: 0;
+                border-radius: 0 3px 3px 0;
+            }
+            .item{
+                border:1px solid #d4d4d4;
+                padding-top:10px;
+                padding-left:10px;
+                padding-right:20px;
+                padding-bottom:10px;
+                margin-bottom: 10px;
+                font-size:12px;
+            }
 
-  <body>
+            .item p{
+                margin-bottom:10px;
+                font-size:12px;
+            }
 
-    <div class="container">
+            .plan{
+                font-size:12px;
+                padding:0px;
+                margin-top:5px;
+                margin-bottom:5px;
+            }
+        </style>
+        <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
 
-      <div class="masthead">
-        <h3 class="muted">Life.log</h3>
-        <div class="navbar">
-          <div class="navbar-inner">
-            <div class="container">
-              <ul class="nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#">Projects</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Downloads</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Contact</a></li>
-              </ul>
+
+    </head>
+
+    <body>
+
+        <div class="container">
+
+            <div class="masthead">
+                <div class="row-fluid">
+                    <div class="span4">
+                        <h3 class="muted logo">Life.log</h3>
+                    </div>
+                    <div class="span4">
+                        <ul>
+                            <li><a href="">Получить новые карточки</a></li>
+                            <li><a href="">Расписаться в новых хренях</a></li>
+                        </ul>
+                    </div>
+                    <div class="span4"> 
+                        <ul>
+                            <li><a href="">Изучить список возможностей</a></li>
+                            <li><a href="">Научить жену жарить картошку</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="navbar">
+                    <div class="navbar-inner">
+                        <div class="container">
+                            <ul class="nav">
+                                <li class="active"><a href="#">Лог</a></li>
+                                <li><a href="#">Инбокс</a></li>
+                                <li><a href="#">Следить</a></li>
+                                <li><a href="#">Проекты</a></li>
+                                <li><a href="#">Когда-нибудь</a></li>
+                                <li><a href="#">Справочник</a></li>
+                                <li><a href="#">Корзина</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div><!-- /.navbar -->
             </div>
-          </div>
-        </div><!-- /.navbar -->
-      </div>
 
-      <!-- Jumbotron -->
-      <div class="jumbotron">
-        <h1>Marketing stuff!</h1>
-        <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-        <a class="btn btn-large btn-success" href="#">Get started today</a>
-      </div>
+            <!-- Example row of columns -->
+            <div class="row-fluid">
+                <div class="span8">
+                    <div class="item">Стоимость обучения в оксфорде 12000$</div>
+                    <div class="item">Выроботка по национальности</div>
+                    <div class="item">Выбрать большое количество временных решений</div>
 
-      <hr>
 
-      <!-- Example row of columns -->
-      <div class="row-fluid">
-        <div class="span4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn" href="#">View details &raquo;</a></p>
-        </div>
-        <div class="span4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn" href="#">View details &raquo;</a></p>
-       </div>
-        <div class="span4">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
-          <p><a class="btn" href="#">View details &raquo;</a></p>
-        </div>
-      </div>
 
-      <hr>
 
-      <div class="footer">
-        <p>&copy; Company 2013</p>
-      </div>
 
-    </div> <!-- /container -->
+                </div>
+                <div class="span4">
+                    <span class="label label-important">Завтра</span>
+                     <div class="plan"><a href="">( 31.06 ) Покатать на машине</a></div>
+                    <span class="label label-important">Через 7 дней</span>
+                    <div class="plan"><a href="">( 05.07 ) Выроботка по национальности</a></div>
+                    <div class="plan"><a href="">( 05.07 )  Активная тектоническая зона быстроспредингового хребта</a></div>
+                    <span class="label label-important">Через 15 дней</span>
+                    <div class="plan"><a href="">( 12.07 ) Выроботка по национальности</a></div>
+                    <div class="plan"><a href="">( 12.07 ) Выбрать большое количество временных решений</a></div>
 
-    <!-- Le javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/latest/jquery.js"></script>
-    <script src="js/bootstrap-transition.js"></script>
-    <script src="js/bootstrap-alert.js"></script>
-    <script src="js/bootstrap-modal.js"></script>
-    <script src="js/bootstrap-dropdown.js"></script>
-    <script src="js/bootstrap-scrollspy.js"></script>
-    <script src="js/bootstrap-tab.js"></script>
-    <script src="js/bootstrap-tooltip.js"></script>
-    <script src="js/bootstrap-popover.js"></script>
-    <script src="js/bootstrap-button.js"></script>
-    <script src="js/bootstrap-collapse.js"></script>
-    <script src="js/bootstrap-carousel.js"></script>
-    <script src="js/bootstrap-typeahead.js"></script>
+                </div>
+            </div>
 
-  </body>
+            <hr>
+
+            <div class="footer">
+                <p>&copy; Company 2013</p>
+            </div>
+
+        </div> <!-- /container -->
+
+        <!-- Le javascript
+        ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/latest/jquery.js"></script>
+        <script src="js/bootstrap-transition.js"></script>
+        <script src="js/bootstrap-alert.js"></script>
+        <script src="js/bootstrap-modal.js"></script>
+        <script src="js/bootstrap-dropdown.js"></script>
+        <script src="js/bootstrap-scrollspy.js"></script>
+        <script src="js/bootstrap-tab.js"></script>
+        <script src="js/bootstrap-tooltip.js"></script>
+        <script src="js/bootstrap-popover.js"></script>
+        <script src="js/bootstrap-button.js"></script>
+        <script src="js/bootstrap-collapse.js"></script>
+        <script src="js/bootstrap-carousel.js"></script>
+        <script src="js/bootstrap-typeahead.js"></script>
+
+    </body>
 </html>
